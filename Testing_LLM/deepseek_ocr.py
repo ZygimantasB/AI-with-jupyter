5364923968e22c863b61040f54b242e42e729c7f
ZygimantasB <brinkis.zygimantas@gmail.com>
2025-10-26 22:04:46 +0200

    Refactored project dependencies, removed `requirements.txt`, added `pyproject.toml` for package management, and integrated DeepSeek OCR model for document processing.

---
+++ Testing_LLM/deepseek_ocr.py
@@ -0,0 +1,16 @@
+from transformers import AutoModel, AutoTokenizer
+import torch
+import os
+
+os.environ["CUDA_VISIBLE_DEVICES"] = '0'
+model_name = 'deepseek-ai/DeepSeek-OCR'
+
+tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+model = AutoModel.from_pretrained(model_name, _attn_implementation='flash_attention_2', trust_remote_code=True, use_safetensors=True)
+model = model.eval().cuda().to(torch.bfloat16)
+
+prompt = "<image>\n<|grounding|>Convert the document to markdown. "
+image_file = r'D:\Programing\AI-with-jupyter\Images\text_basics_docs_final_document.jpg'
+output_path = r'D:\Programing\AI-with-jupyter\Images\output'
+
+res = model.infer(tokenizer, prompt=prompt, image_file=image_file, output_path = output_path, base_size = 1024, image_size = 640, crop_mode=True, save_results = True, test_compress = True)
